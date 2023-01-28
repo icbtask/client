e57b641a5b9b59945a14e0f2dde93fe8179e23e6
pythops <contact@pythops.com>
2023-01-28 21:59:14 +0100

    Add default base url

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,12 @@
 use serde::Deserialize;
 
+fn default_url() -> String {
+    String::from("https://api.icbtask.com")
+}
+
 #[derive(Deserialize, Debug)]
 pub struct Config {
     pub base_url: String,
+    #[serde(default = "default_url")]
     pub api_key: String,
 }
